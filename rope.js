744476a4698b3d5b0780eb89256d51ed7c850812
Krishnapriya12345678 <72981926+Krishnapriya12345678@users.noreply.github.com>
2020-11-24 19:27:02 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,35 @@
+class Rope{
+    constructor(bodyA, bodyB,offsetX){
+       
+        var options = {
+            bodyA: bodyA,
+            bodyB:bodyB,
+           stiffness: 0.02,
+            length: 20
+        }
+        
+        this.offsetX=100
+        World.add(world,this.offsetX)
+        this.Rope = Matter.Constraint.create(options);
+        World.add(world, this.Rope);
+    }
+ 
+    display(){
+       var   posA = this.Rope.bodyA.position;
+       var   posB= this.Rope.bodyB.position
+      strokeWeight(4);
+        line(posA.x, posA.y, posB.x+this.offsetX,posB.y);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
